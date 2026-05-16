10feccdb5690f5c583f1335146352e83ec72130b
Girish Lade <girishlade111@gmail.com>
2026-05-16 12:52:41 -0700

    feat: add dashboard pages for assets and invoice creation

--- apps/web/src/app/(dashboard)/assets/page.tsx
+++ apps/web/src/app/(dashboard)/assets/page.tsx
@@ -1,5 +1,12 @@
 import AssetsPage from "./assets";
+import { generateWebsiteMetadata } from "@/constants/meta-data";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = generateWebsiteMetadata({
+  title: "Assets",
+  noindex: true,
+});
 
 const Page = () => {
   return <AssetsPage />;
--- apps/web/src/app/(dashboard)/create/invoice/page.tsx
+++ apps/web/src/app/(dashboard)/create/invoice/page.tsx
@@ -1,5 +1,12 @@
 import InvoicePage from "./invoice";
+import { generateWebsiteMetadata } from "@/constants/meta-data";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = generateWebsiteMetadata({
+  title: "Create Invoice",
+  noindex: true,
+});
 
 const Page = () => {
   return <InvoicePage />;
